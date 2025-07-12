b57617c02412aaacd07ca9b1f7754e554d265e05
Clara Battesini <clarabattesini@gmail.com>
2025-07-11 20:42:00 -0400

    Update database connection to use environment variable for host

--- backend/db/connection.py
+++ backend/db/connection.py
@@ -6,12 +6,13 @@ dotenv_path = find_dotenv()
 
 load_dotenv(dotenv_path)
 
+host = os.getenv("host")
 user = os.getenv("user")
 password = os.getenv("password")
 database = os.getenv("database")
 
 connection = mysql.connector.connect(
-    host="localhost",
+    host=host,
     user= user,
     password =password,
     database=database
